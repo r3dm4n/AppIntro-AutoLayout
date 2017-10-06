58bad9e124d746ae4140653a89efde57100395f9
Alexandru Corut <r3dm4n@me.com>
2017-10-06 09:11:56 +0100

    added image and textview

--- auto-layout-programmatically/ViewController.swift
+++ auto-layout-programmatically/ViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let bearImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_first"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let descriptionTextView: UITextView = {
+       let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "Join us today in our fun and games"
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        
+        view.addSubview(bearImageView)
+        view.addSubview(descriptionTextView)
+        setupLayout()
     }
-
-
-
+    
+    private func setupLayout() {
+        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100 ).isActive = true
+        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
+        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+    }
+    
 }
 

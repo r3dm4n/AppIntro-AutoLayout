25c0ce7545b361b40ba9bbbf921ec082137b7375
r3dm4n <r3dm4n@me.com>
2017-11-04 13:44:59 +0000

    added UIColor extension

---
+++ auto-layout-programmatically/Extensions/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  auto-layout-programmatically
+//
+//  Created by Alexandru Corut on 04/11/2017.
+//  Copyright © 2017 Alexandru Corut. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
